1d0fc70f0338f8214bc9d338fbc6111269f65fc3
shahedaltrakiah <shahedaltrakiah@gmail.com>
2024-10-19 01:26:40 +0300

    up

--- includes/db.php
+++ includes/db.php
@@ -4,11 +4,11 @@ $username = "root";
 $password = "";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=mydatabase", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=ecommerce_db", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
-} catch(PDOException $e) {
+} catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
 ?>
