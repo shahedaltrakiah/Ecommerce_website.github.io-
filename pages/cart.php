b0050c2392faee05c7acdd5f539b34dfc5f47038
shahedaltrakiah <shahedaltrakiah@gmail.com>
2024-10-22 00:21:45 +0300

    upp

--- pages/cart.php
+++ pages/cart.php
@@ -113,36 +113,27 @@ if (!isset($_SESSION['redirected'])) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="author" content="Untree.co">
-	<link rel="shortcut icon" href="../images/logo">
+  <link rel="shortcut icon" href="../images/logo">
   <meta name="description" content="" />
   <meta name="keywords" content="bootstrap, bootstrap4" />
 
-<<<<<<< HEAD
   <!-- Bootstrap CSS -->
   <link href="../css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
   <link href="../css/tiny-slider.css" rel="stylesheet">
   <link href="../css/style.css" rel="stylesheet">
-=======
-		<!-- Bootstrap CSS -->
-		<link href="../css/bootstrap.min.css" rel="stylesheet">
-		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-		<link href="../css/tiny-slider.css" rel="stylesheet">
-        <link href="../css/style.css" rel="stylesheet">
-        
->>>>>>> 6fc97aa9fa36afbb92024fbe0142732ac2d36acb
 
   <title>Furni Free Bootstrap 5 Template for Furniture and Interior Design Websites by Untree.co </title>
 </head>
 
 <body>
 
-<<<<<<< HEAD
   <!-- Start Header/Navigation -->
   <nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
     <div class="container">
-      <a class="navbar-brand" href="index.html">Furni<span>.</span></a>
+      <a class="navbar-brand" href="../index.php"><img class="logo-img" src="../images/Logo"
+          style="max-width:150px;"></a>
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni"
         aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
@@ -152,36 +143,12 @@ if (!isset($_SESSION['redirected'])) {
       <div class="collapse navbar-collapse" id="navbarsFurni">
         <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
           <li class="nav-item ">
-            <a class="nav-link" href="../index.php ">Home</a>
+            <a class="nav-link" href="../index.php">Home</a>
           </li>
-          <li><a class="nav-link" href="../pages/shop.php">Shop</a></li>
+          <li><a class="nav-link" href="shop.php">Shop</a></li>
           <li><a class="nav-link" href="about.html">About us</a></li>
-          <li><a class="nav-link" href="services.html">Services</a></li>
-          <li><a class="nav-link" href="../pages/contact.php">Contact us</a></li>
+          <li><a class="nav-link" href="contact.php">Contact us</a></li>
         </ul>
-=======
-		<!-- Start Header/Navigation -->
-    <nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
-
-<div class="container">
-  <a class="navbar-brand" href="../index.php"><img class="logo-img" src="../images/Logo"
-      style="max-width:150px;"></a>
-
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni"
-    aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarsFurni">
-    <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-      <li class="nav-item ">
-        <a class="nav-link" href="../index.php">Home</a>
-      </li>
-      <li><a class="nav-link" href="shop.php">Shop</a></li>
-      <li><a class="nav-link" href="about.html">About us</a></li>
-      <li><a class="nav-link" href="contact.php">Contact us</a></li>
-    </ul>
->>>>>>> 6fc97aa9fa36afbb92024fbe0142732ac2d36acb
 
         <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
           <li><a class="nav-link" href="login.php"><i class="fa-solid fa-right-to-bracket"></i></a></li>
@@ -192,36 +159,35 @@ if (!isset($_SESSION['redirected'])) {
         </ul>
       </div>
     </div>
-<<<<<<< HEAD
-=======
-    <form method="post" class="d-flex flex-column p-2">
-        <div class="col-md-8 mb-3 mb-md-0 ">
-            <input type="text" class="form-control py-3" id="coupon" name="coupon_code" placeholder="Coupon Code">
-        </div>
-        <div class="col-md-4 mt-2">
-            <button type="submit" name="apply_coupon" class="btn btn-black">Apply Coupon</button>
-        </div>
-    </form>
-</div>
-                </div>
-                <div class="col-md-6 pl-5">
-                  <div class="row justify-content-end">
-                    <div class="col-md-7">
-                      <div class="row">
-                        <div class="col-md-12 text-right border-bottom mb-5">
-                          <h3 class="text-black h4 text-uppercase">Cart Totals</h3>
-                        </div>
-                      </div>
-                      <?php if (isset($_SESSION['error_message'])): ?>
-    <div class="alert alert-danger">
-        <?php echo $_SESSION['error_message']; ?>
-        <?php unset($_SESSION['error_message']); // Clear the message after displaying ?>
-    </div>
-<?php endif; ?>
->>>>>>> 6fc97aa9fa36afbb92024fbe0142732ac2d36acb
 
   </nav>
   <!-- End Header/Navigation -->
+
+  <!-- <form method="post" class="d-flex flex-column p-2">
+    <div class="col-md-8 mb-3 mb-md-0 ">
+      <input type="text" class="form-control py-3" id="coupon" name="coupon_code" placeholder="Coupon Code">
+    </div>
+    <div class="col-md-4 mt-2">
+      <button type="submit" name="apply_coupon" class="btn btn-black">Apply Coupon</button>
+    </div>
+  </form>
+
+  <div class="col-md-6 pl-5">
+    <div class="row justify-content-end">
+      <div class="col-md-7">
+        <div class="row">
+          <div class="col-md-12 text-right border-bottom mb-5">
+            <h3 class="text-black h4 text-uppercase">Cart Totals</h3>
+          </div>
+        </div>
+        <?php if (isset($_SESSION['error_message'])): ?>
+          <div class="alert alert-danger">
+            <?php echo $_SESSION['error_message']; ?>
+            <?php unset($_SESSION['error_message']); // Clear the message after displaying ?>
+          </div>
+        <?php endif; ?> -->
+
+
 
   <div class="untree_co-section before-footer-section">
     <div class="container">
@@ -354,7 +320,6 @@ if (!isset($_SESSION['redirected'])) {
               </div>
             </div>
           </div>
-<<<<<<< HEAD
         </div>
       </div>
     </div>
@@ -396,8 +361,11 @@ if (!isset($_SESSION['redirected'])) {
       <div class="row g-5 mb-5">
         <div class="col-lg-4">
           <div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Furni<span>.</span></a></div>
-          <p class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada.
-            Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant</p>
+          <p class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus
+            malesuada.
+            Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque
+            habitant
+          </p>
 
           <ul class="list-unstyled custom-social">
             <li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
@@ -451,9 +419,11 @@ if (!isset($_SESSION['redirected'])) {
         <div class="row pt-4">
           <div class="col-lg-6">
             <p class="mb-2 text-center text-lg-start">Copyright &copy;
-              <script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash; Designed with
+              <script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash; Designed
+              with
               love by <a href="https://untree.co">Untree.co</a> Distributed By <a
-                hreff="https://themewagon.com">ThemeWagon</a> <!-- License information: https://untree.co/license/ -->
+                hreff="https://themewagon.com">ThemeWagon</a>
+              <!-- License information: https://untree.co/license/ -->
             </p>
           </div>
 
@@ -470,96 +440,96 @@ if (!isset($_SESSION['redirected'])) {
     </div>
   </footer>
   <!-- End Footer Section -->
-=======
-		
-	<!-- Start Footer Section -->
-	<footer class="footer-section">
-		<div class="container relative" style="margin-bottom: -50px;">
+  =======
 
-			<div class="sofa-img">
-				<img src="../images/sofa.png" alt="Image" class="img-fluid">
-			</div>
+  <!-- Start Footer Section -->
+  <footer class="footer-section">
+    <div class="container relative" style="margin-bottom: -50px;">
 
-			<div class="row">
-				<div class="col-lg-8">
-					<div class="subscription-form">
-					</div>
-				</div>
-			</div>
+      <div class="sofa-img">
+        <img src="../images/sofa.png" alt="Image" class="img-fluid">
+      </div>
 
-			<div class="row g-5 mb-5">
-				<div class="col-lg-4">
-					<div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Nest & Buy </a></div>
-					<p class="mb-4">At Nest & Buy, we bring you stylish, high-quality furniture to make your home cozy
-						and beautiful. With a focus on craftsmanship and design, we’re here to help you create spaces
-						you love.
-					</p>
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="subscription-form">
+          </div>
+        </div>
+      </div>
 
-					<ul class="list-unstyled custom-social">
-						<li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
-						<li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
-						<li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
-						<li><a href="#"><span class="fa fa-brands fa-linkedin"></span></a></li>
-					</ul>
-				</div>
+      <div class="row g-5 mb-5">
+        <div class="col-lg-4">
+          <div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Nest & Buy </a></div>
+          <p class="mb-4">At Nest & Buy, we bring you stylish, high-quality furniture to make your home cozy
+            and beautiful. With a focus on craftsmanship and design, we’re here to help you create spaces
+            you love.
+          </p>
 
-				<div class="col-lg-8">
-					<div class="row links-wrap">
-						<div class="col-6 col-sm-6 col-md-3">
-							<ul class="list-unstyled">
-								<li><a href="about.html">About us</a></li>
-								<li><a href="shop.php">Shop</a></li>
-								<li><a href="#">Contact us</a></li>
-							</ul>
-						</div>
+          <ul class="list-unstyled custom-social">
+            <li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
+            <li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
+            <li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
+            <li><a href="#"><span class="fa fa-brands fa-linkedin"></span></a></li>
+          </ul>
+        </div>
 
-						<div class="col-6 col-sm-6 col-md-3">
-							<ul class="list-unstyled">
-								<li><a href="#">Testimonials</a></li>
-								<li><a href="about.html#ourTeam">Our team</a></li>
-								<li><a href="#">Live chat</a></li>
-							</ul>
-						</div>
+        <div class="col-lg-8">
+          <div class="row links-wrap">
+            <div class="col-6 col-sm-6 col-md-3">
+              <ul class="list-unstyled">
+                <li><a href="about.html">About us</a></li>
+                <li><a href="shop.php">Shop</a></li>
+                <li><a href="#">Contact us</a></li>
+              </ul>
+            </div>
 
-						<div class="col-6 col-sm-6 col-md-3">
-							<ul class="list-unstyled">
-								<li><a href="#">Category</a></li>
-								<li><a href="#">Product</a></li>
-								<li><a href="#">Best seller</a></li>
-							</ul>
-						</div>
+            <div class="col-6 col-sm-6 col-md-3">
+              <ul class="list-unstyled">
+                <li><a href="#">Testimonials</a></li>
+                <li><a href="about.html#ourTeam">Our team</a></li>
+                <li><a href="#">Live chat</a></li>
+              </ul>
+            </div>
 
-						<div class="col-6 col-sm-6 col-md-3">
-							<ul class="list-unstyled">
-								<img src="../images/Logo (2).png" style="max-width: 130px;">
-							</ul>
-						</div>
-					</div>
-				</div>
+            <div class="col-6 col-sm-6 col-md-3">
+              <ul class="list-unstyled">
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Product</a></li>
+                <li><a href="#">Best seller</a></li>
+              </ul>
+            </div>
 
-			</div>
+            <div class="col-6 col-sm-6 col-md-3">
+              <ul class="list-unstyled">
+                <img src="../images/Logo (2).png" style="max-width: 130px;">
+              </ul>
+            </div>
+          </div>
+        </div>
 
-			<div class="border-top copyright">
-				<div class="row pt-4">
-					<div class="col-lg-6">
-						<p class="mb-2 text-center text-lg-start">Copyright &copy;
-							<script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash;
-							Designed with love by Nest & Buy
-						</p>
-					</div>
+      </div>
 
-					<div class="col-lg-6 text-center text-lg-end">
-						<ul class="list-unstyled d-inline-flex ms-auto">
-							<li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
-							<li><a href="#">Privacy Policy</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
-	<!-- End Footer Section -->
->>>>>>> 6fc97aa9fa36afbb92024fbe0142732ac2d36acb
+      <div class="border-top copyright">
+        <div class="row pt-4">
+          <div class="col-lg-6">
+            <p class="mb-2 text-center text-lg-start">Copyright &copy;
+              <script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash;
+              Designed with love by Nest & Buy
+            </p>
+          </div>
+
+          <div class="col-lg-6 text-center text-lg-end">
+            <ul class="list-unstyled d-inline-flex ms-auto">
+              <li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- End Footer Section -->
+  >>>>>>> 6fc97aa9fa36afbb92024fbe0142732ac2d36acb
 
 
   <script src="../js/bootstrap.bundle.min.js"></script>
