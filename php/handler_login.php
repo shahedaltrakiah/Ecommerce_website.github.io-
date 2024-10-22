6d8774f24e6cb38fdfd930ac03f159336bcce319
Mousa-alaaldeen <azmimousa396@gmail.com>
2024-10-22 17:15:32 +0300

    end login

--- php/handler_login.php
+++ php/handler_login.php
@@ -4,38 +4,45 @@ include_once("../includes/request.php");
 include_once("../includes/validations.php");
 include_once("../includes/sessions.php");
 include_once('../includes/db.php');
-$errors = [];
-if (postMethod()) {  
-    foreach ($_POST as $key => $value) {
-        $$key = receivedInput($value);
-    }
-    try {
-        $query = "SELECT password, username FROM Customers WHERE email = :email";
-        $stmt = $conn->prepare($query);
-        $stmt->bindParam(":email", $email);
-        $stmt->execute();
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
-      
-        if ($user && password_verify($password, $user['password'])) {
-          
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {  
+  
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+
+    try {
+        $query ="SELECT password, username, image_url, customer_ID FROM Customers WHERE email = :email";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':image_url', $image_url); 
+        $stmt->execute();
+
+     
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        
+        if ($user && password_verify($password, $user['password'])) {
+           
             $_SESSION['user'] = [
-                'name' =>  $user['username'],
+                'name' => $user['username'],
                 'email' => $email,
                 'image_url' => $user['image_url'],
-                'customer_ID'=> $user['customer_ID'],
+                'customer_ID' => $user['customer_ID'],
             ];
+           
+            header("Location: ../index.php");
             exit();
-        } 
+        } else {
+        
+            
+            echo '<script>window.location.href = "../pages/login.php";</script>';
+            exit();
+        }
     } catch (Exception $e) {
-        $errors[] = $e->getMessage();
-    }
-
-    if (!empty($errors)) {
-        sessionStore("signInErrors", $errors);
-        echo '<script>
-                window.location.href = "../pages/login.php";
-              </script>';
+     
+     
+        echo '<script>window.location.href = "../pages/login.php";</script>';
         exit();
     }
 } else {
